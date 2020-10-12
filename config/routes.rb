ff1b6459759395c58ba458596467fea29c1e7875
kawarimidoll <kawarimidoll+git@gmail.com>
2020-10-12 11:47:26 +0900

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'svg/today', format: :svg
+  get 'today', to: 'svg#today', format: :svg
   # username regexp: https://github.com/shinnn/github-username-regex
-  get 'svg/recent_contrib/:username', to: 'svg#recent_contrib', username: /[a-z\d]([a-z\d]|-[a-z\d]){0,38}/
+  get 'recent_contrib/:username', to: 'svg#recent_contrib', username: /[a-z\d]([a-z\d]|-[a-z\d]){0,38}/
 end
